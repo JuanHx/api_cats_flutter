3c5fa704fe153fce84496fb1fd698dafc0fe8327
JuanHX <juan.henao@supergiros.com.co>
2025-08-01 00:39:00 -0500

    se elimina repositorio sin uso

--- lib/domain/repositories/cat_repositories.dart
+++
@@ -1,8 +0,0 @@
-// domain/repositories/cat_repository.dart
-import '../entities/breed.dart';
-import '../entities/image_cat.dart';
-
-abstract class CatRepository {
-  Future<List<Breed>> getAllBreeds();
-  Future<List<ImageCat>> getImagesByBreed(String breedId, {int limit});
-}
